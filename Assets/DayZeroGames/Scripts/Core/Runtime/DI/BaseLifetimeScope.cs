1f703ef168c1ccad9b2e07fff848334457180fb9
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-06-28 12:46:49 +0545

    improvement: minor debug log improvement

--- Assets/DayZeroGames/Scripts/Core/Runtime/DI/BaseLifetimeScope.cs
+++ Assets/DayZeroGames/Scripts/Core/Runtime/DI/BaseLifetimeScope.cs
@@ -11,11 +11,16 @@ namespace DZ.Core.Runtime
         protected override void Configure(IContainerBuilder builder)
         {
 	        if (featureInstallers.Length == 0) return;
+	        
             foreach (var featureInstaller in featureInstallers)
             {
                 if (featureInstaller!=null)
                 {
                     featureInstaller.Register(builder);
+                }
+                else
+                {
+	                Debug.LogWarning("Feature Installer Not Found! Maybe you forgot to assign a feature installer?");
                 }
             }
         }
