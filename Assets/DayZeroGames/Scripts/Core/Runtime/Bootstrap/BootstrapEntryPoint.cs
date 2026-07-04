16f4c0d86c21ea7f73a2a5d5e3153ed4d1ff3688
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-07-04 23:12:18 +0545

    feature: added BootstrapEntryPoint | guard against duplicate RootLifetimeScope

---
+++ Assets/DayZeroGames/Scripts/Core/Runtime/Bootstrap/BootstrapEntryPoint.cs
@@ -0,0 +1,12 @@
+using VContainer.Unity;
+
+namespace DZ.Core
+{
+	public abstract class BootstrapEntryPoint : IStartable
+	{
+		public void Start()
+		{
+			// TODO: Load MainMenu scene here
+		}
+	}
+}
