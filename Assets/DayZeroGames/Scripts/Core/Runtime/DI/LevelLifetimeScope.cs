c0873f9daf441359cbaf45099c0cf90da0bfefa5
Mausham <neupanemausham2060@gmail.com>
2026-07-28 13:35:13 +0545

    added level lifetime scope

---
+++ Assets/DayZeroGames/Scripts/Core/Runtime/DI/LevelLifetimeScope.cs
@@ -0,0 +1,13 @@
+using DZ.Core.Runtime;
+using VContainer;
+using VContainer.Unity;
+
+namespace DZ.Core
+{
+    public class LevelLifetimeScope : BaseLifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
