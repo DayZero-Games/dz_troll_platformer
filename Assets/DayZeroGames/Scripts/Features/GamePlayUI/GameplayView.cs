dd9ee1726dbd9bd40f724aa974801c9d0263caf2
Mausham Neupane <neupanemausham2060@gmail.com>
2026-08-04 23:04:53 +0545

    added UI in game

---
+++ Assets/DayZeroGames/Scripts/Features/GamePlayUI/GameplayView.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace DZ.Features
+{
+    public class GameplayView : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
